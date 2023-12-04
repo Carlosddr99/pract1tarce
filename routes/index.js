161e3ab0dadccb6d8f3b5a4444be72562432b201
Carlos Daniel <carlosddr1999@hotmail.com>
2023-12-04 11:55:56 +0000

    error app

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-app.use(express.static("images")); 
+router.use(express.static("images")); 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
