c84cdc9629c136d763e40e6f7ddea3e9ceae5bc9
Carlos Daniel <carlosddr1999@hotmail.com>
2023-12-14 13:01:18 +0000

    Cambios render

--- routes/miRutaNueva.js
+++ routes/miRutaNueva.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.use(express.static("images")); 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
